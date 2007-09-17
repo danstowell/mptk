1ad5d3879f3fd760f0e99792d290bfffb1be573c
broy <broy@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2007-09-17 08:53:56 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1158 0f77665a-23fb-0310-bc23-e91fd8efc3e1

--- plugin_system/src/matlab/dictread.m
+++
@@ -1,6 +0,0 @@
-% DICTREAD Imports a dictionary Matching Pursuit dict in Matlab
-%  dict = dictread(filename)
-% 
-% Distributed under the General Public License.
-%                                       
-%#mex
