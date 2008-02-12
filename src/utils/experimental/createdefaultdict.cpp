a88270cf6b4a3c1ad8d6e156892135d80553e288
gonon <gonon@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2008-02-12 09:55:30 +0000

    changed createdefaultdict.cpp to allow specification of input name
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1208 0f77665a-23fb-0310-bc23-e91fd8efc3e1

--- src/utils/experimental/createdefaultdict.cpp
+++ src/utils/experimental/createdefaultdict.cpp
@@ -11,13 +11,19 @@
 //void printParamMap(map<string, string, mp_ltstring> * paramMap );
 
 char* func = "create";
+char* pn = "";  /* program name */
 
+/* Usage help */
+void usage() {
+  fprintf (stdout,"%s -- Creates a default dictionnary using all known blocks in mptk\n",pn);
+  fprintf (stdout,"  usage: %s -- Creates the default dictionnary in the current folder\n",pn);
+  fprintf (stdout,"         %s dictName.xml -- Creates the default dictionnary in file dictName.xml\n\n",pn);
+  return;
+}
 
-
-
+/* Print block parameters information */
 void printParamMap(map<string, string, mp_ltstring> * paramMap )
 {
-
   map<string, string, mp_ltstring>::iterator paramIterator;
   /*Iterate on varparam list to list all parameters map for blocks*/
   cout << "--" << endl;
@@ -29,12 +35,35 @@ void printParamMap(map<string, string, mp_ltstring> * paramMap )
   cout << "--" << endl;
 }
 
+/* Main function */
 int main(int argc, char **argv)
 {
+
+  char * dictName;
+  dictName = (char*) malloc (512+1);
+  pn = argv[0]; /* set program name in global var */
+  // Parse command line
+  if (argc==1) 
+    {
+      strcpy(dictName,"MPdefaultDictionnary.xml");
+    }
+  else
+    {
+      if ( (0==strcmp(argv[1],"-h")) || (0==strcmp(argv[1],"-help")) || (0==strcmp(argv[1],"--help")) ) 
+	{
+	  usage();
+	  return(0);
+	}
+      else
+	{
+	  dictName = argv[1];
+	}
+    }
+  
   // Charge l'environnement MPTK
   if (!MPTK_Env_c::get_env()->get_environment_loaded())
     MPTK_Env_c::get_env()->load_environment("");
-
+  
   // Affiche la liste des Blocks
   mp_info_msg( func,"The following Block types have been successfully loaded:\n");
   vector< string >* nameVector = new vector< string >();
@@ -43,10 +72,9 @@ int main(int argc, char **argv)
     {
       cout << "block " << i << ":" << nameVector->at(i) << endl;
     }
+  
 
-
-  // Define a default mono signal
-
+  // Define a default stereo signal
   MP_Signal_c * signal = NULL;
   signal = MP_Signal_c::init(2,48000,48000);
   if (NULL == signal)
@@ -57,73 +85,68 @@ int main(int argc, char **argv)
     {
       cout << "Signal initialized" << endl;
     }
-
+  
   // Create a default dict with default blocks ...
   MP_Dict_c * myDict = NULL;
   myDict = MP_Dict_c::init();
-
+  
   // Create a default bloc for each block type and display info about it
   for (unsigned int i= 0; i < nameVector->size(); i++)
     {
-if (strcmp(nameVector->at(i).c_str(),"anywavehilbert") and strcmp(nameVector->at(i).c_str(),"anywave")){
-      cout << "Try to create a default block for type: [" << nameVector->at(i) << "]" << endl;
-      myDict->add_default_block(nameVector->at(i).c_str());
-      cout << "... it seems ok" << endl;
-
-
-      MP_Block_c *newBlock = NULL;
-      map<string, string, mp_ltstring>* defaultMap = new map<string, string, mp_ltstring>();
-      MP_Block_Factory_c::get_block_factory()->get_block_default_map(nameVector->at(i).c_str())(defaultMap);
-
-      /*call the block creator*/
-      MP_Block_c* (*blockCreator)( MP_Signal_c *setSignal, map<string, string, mp_ltstring> * paramMap ) = NULL;
-      blockCreator = MP_Block_Factory_c::get_block_factory()->get_block_creator(nameVector->at(i).c_str());
-
-      if (NULL == blockCreator)
-        {
-          cout << "[" << nameVector->at(i) << "]: this block type is not registred in the atom factory" << endl;
-          if (defaultMap) delete(defaultMap);
-          return 0;
-        }
-      else
-        {
-          cout << "[" << nameVector->at(i) << "]: succesfully affected default parameter by the atom factory" << endl;
-        }
-
-      printParamMap(defaultMap);
-//    string key();
-//    string val();
-//    defaultMap["f0Min"] = 50.0;
-      //defaultMap["numPartials"] = string("10");
-
-      /*Create a new block*/
-      newBlock =  blockCreator(signal, defaultMap);
-      cout << "Here" << endl;
-
-      /*Test if new block is NULL*/
-      if (NULL == newBlock)
-        {
-          cout << "Failed to create the block of type " << nameVector->at(i) << endl;
-          if (defaultMap) delete(defaultMap);
-          return 0;
-        }
-      else
-        {
-          // Print Block info
-          cout << " Block of type [" << nameVector->at(i) << "] successfully created" << endl;
-        }
-      // Delete loop objects
-      if (defaultMap) delete(defaultMap);
-//    if (newBlock) delete(newBlock);
-
-}
+      if (strcmp(nameVector->at(i).c_str(),"anywavehilbert") and strcmp(nameVector->at(i).c_str(),"anywave")){
+	cout << "Try to create a default block for type: [" << nameVector->at(i) << "] ... ";
+	myDict->add_default_block(nameVector->at(i).c_str());
+	cout << " OK" << endl;
+	
+	
+	MP_Block_c *newBlock = NULL;
+	map<string, string, mp_ltstring>* defaultMap = new map<string, string, mp_ltstring>();
+	MP_Block_Factory_c::get_block_factory()->get_block_default_map(nameVector->at(i).c_str())(defaultMap);
+	
+	/*call the block creator*/
+	MP_Block_c* (*blockCreator)( MP_Signal_c *setSignal, map<string, string, mp_ltstring> * paramMap ) = NULL;
+	blockCreator = MP_Block_Factory_c::get_block_factory()->get_block_creator(nameVector->at(i).c_str());
+	
+	if (NULL == blockCreator)
+	  {
+	    cout << "[" << nameVector->at(i) << "]: this block type is not registred in the atom factory" << endl;
+	    if (defaultMap) delete(defaultMap);
+	    return 0;
+	  }
+	else
+	  {
+	    cout << "[" << nameVector->at(i) << "]: succesfully affected default parameter by the atom factory" << endl;
+	  }
+	
+	printParamMap(defaultMap);
+	
+	/*Create a new block*/
+	newBlock =  blockCreator(signal, defaultMap);
+	cout << "Here" << endl;
+	
+	/*Test if new block is NULL*/
+	if (NULL == newBlock)
+	  {
+	    cout << "Failed to create the block of type " << nameVector->at(i) << endl;
+	    if (defaultMap) delete(defaultMap);
+	    return 0;
+	  }
+	else
+	  {
+	    // Print Block info
+	    cout << " Block of type [" << nameVector->at(i) << "] successfully created" << endl;
+	  }
+	// Delete loop objects
+	if (defaultMap) delete(defaultMap);
+	if (newBlock) delete(newBlock);
+      }
     }
 
-//  myDict->print("totodict.xml");
-
+  myDict->print(dictName);
   delete(nameVector);
+  // Erase dictionnary
+  myDict->delete_all_blocks();
   delete(myDict);
-
 
   return 0;
 }
