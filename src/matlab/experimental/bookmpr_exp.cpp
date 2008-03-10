cb693b024fc6bdf8518358fb43d1056e0c1839a8
gonon <gonon@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2008-03-10 16:48:53 +0000

    Matlab-experimentals: Adding bookmpr_exp.cpp -- This file gives an segmentation fault under matlab. This seems to be caused by the thread start for MP_Atom_c::substract_add(). Hope future version of matlab won't give this error ...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1218 0f77665a-23fb-0310-bc23-e91fd8efc3e1

---
+++ src/matlab/experimental/bookmpr_exp.cpp
@@ -0,0 +1,75 @@
+/******************************************************************************/
+/*                                                                            */
+/*                  	      bookmpr_exp.c                                   */
+/*                                                                            */
+/*      		    mptkMEX toolbox			              */
+/*                                                                            */
+/* Gilles Gonon                                                 March 07 2008 */
+/* -------------------------------------------------------------------------- */
+/*                                                                            */
+/*  This program is free software; you can redistribute it and/or             */
+/*  modify it under the terms of the GNU General Public License               */
+/*  as published by the Free Software Foundation; either version 2            */
+/*  of the License, or (at your option) any later version.                    */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful,           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with this program; if not, write to the Free Software               */
+/*  Foundation, Inc., 59 Temple Place - Suite 330,                            */
+/*  Boston, MA  02111-1307, USA.                                              */
+/*                                                                            */
+/******************************************************************************/
+/*
+ * $Version 0.5.4$
+ * $Date 07/03/2008$
+ */
+
+#include "mex.h"
+#include "mxBook.h"
+
+
+/*
+ *
+ *     MAIN MEX FUNCTION
+ *
+ */
+void mexFunction(int nlhs, mxArray *plhs[],
+int nrhs, const mxArray *prhs[]) {
+    
+    /* Check input arguments */
+    if (nrhs != 1) {
+        mexPrintf("!!! %s error -- bad number of input arguments\n",mexFunctionName());
+        mexPrintf("    see help %s\n",mexFunctionName());
+        return;
+    }
+ 
+    if ( !mxIsStruct(prhs[0])) {
+      mexPrintf("!!! %s error -- At least one argument has a wrong type\n",mexFunctionName());
+      mexPrintf("    see help %s\n",mexFunctionName());
+      return;        
+    }
+       
+    /** Check output args */
+    if (nlhs != 1) {
+        mexPrintf("!!! %s error -- bad number of output arguments\n",mexFunctionName());
+        mexPrintf("    see help %s\n",mexFunctionName());
+        return;
+    }
+    
+    /* Load the MPTK environment if not loaded */
+    if (!MPTK_Env_c::get_env()->get_environment_loaded())MPTK_Env_c::get_env()->load_environment("");
+
+    /** Load book structure in object */
+    mxBook mybook(prhs[0]);
+    mxArray *mxOut = NULL;
+    
+    mxOut = mybook.Book_Reconstruct();
+    
+    plhs[0] = mxOut;
+
+    return;
+}
---
+++ src/matlab/experimental/bookmpr_exp.m
@@ -0,0 +1,17 @@
+% BOOKREAD_EXP Imports a binary Matching Pursuit book in Matlab
+%
+%    book = BOOKREAD_EXP( 'fileName' ) reads the binary format book
+%    file 'fileName' and returns it as a structure.
+%
+%    The following blocks are supported: 
+%    gabor, harmonic, mdct, mdst, mclt, dirac, constant, nyquist
+%
+%    See also BOOKWRITE_EXP, BOOKEDIT_EXP.
+
+%
+% Author:
+% Gilles Gonon
+% 
+% Distributed under the General Public License.
+%                                       
+%#mex
