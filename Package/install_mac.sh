04281f859b6480d0eb95648077702f7134cda495
rleboulc <rleboulc@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2010-04-13 10:13:05 +0000

    Commit de Ronan Le Boulch : - Livraison des fichiers permettant l'installation du package avec Linux
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1404 0f77665a-23fb-0310-bc23-e91fd8efc3e1

---
+++ Package/install_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "moving files..."
+cp -a ./bin/ /usr/local/bin/;
+cp -a ./include/ /usr/local/include/;
+cp -a ./lib/ /usr/local/lib;
+cp -a ./mptk/ /usr/local/mptk/;
+echo "updating dynamic link libraries..."
+echo "purging temp files..."
+rm -rf ../MPTK-0.5.7-Linux
+echo "MPTK has extracted itself."
