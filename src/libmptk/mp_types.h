573d9584033c6647cd0f7d050958a3e61f18e70a
rleboulc <rleboulc@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2010-05-25 15:12:25 +0000

    Commit de Ronan Le Boulch : Modification des types pour windows afin de palier à l'erreur provoquée par l'arrivée de stdint.h qui n'est pas reconnu par Visual Studio
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1476 0f77665a-23fb-0310-bc23-e91fd8efc3e1

--- src/libmptk/mp_types.h
+++ src/libmptk/mp_types.h
@@ -46,8 +46,18 @@
 #define __mp_types_h_
 
 #include <stdlib.h>
-#include <stdint.h>
 #include <string>
+
+#ifdef _MSC_VER
+
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int32 uint64_t;
+
+#else
+#include <stdint.h>
+#endif
 
 using namespace std;
 
