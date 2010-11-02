ff2a91452805d6e58f73d8bd68fc3ae0f5dd3410
rleboulc <rleboulc@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2010-11-02 08:29:14 +0000

    Commit de Ronan Le Boulch : - Modification de l'utilisation du serveur PIPOL
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1684 0f77665a-23fb-0310-bc23-e91fd8efc3e1

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,31 +5,3 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.inria.fr")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=MPTK")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
-#--------------------------------------------------------------------
-# BUILNAME variable construction
-# This variable will be used to set the build name which will appear 
-# on the MPTK dashboard http://cdash.irisa.fr/CDash/
-#--------------------------------------------------------------------
-# Start with the short system name, e.g. "Linux", "FreeBSD" or "Windows"
-IF(BUILDNAME)
- SET(BUILDNAME "${BUILDNAME}-${CMAKE_SYSTEM_NAME}")
-ELSE(BUILDNAME)
-  # To suppress the first space if BUILDNAME is not set
- SET(BUILDNAME "${CMAKE_SYSTEM_NAME}")
-ENDIF(BUILDNAME)
-
-# Add the compiler name, e.g. "g++, ..."
-SET(BUILDNAME "${BUILDNAME}-${CMAKE_BASE_NAME}")
-
-# Add the type of library generation, e.g. "Dynamic or Static"
-IF(BUILD_SHARED_LIBS)
- SET(BUILDNAME "${BUILDNAME}-Dynamic")
-ELSE(BUILD_SHARED_LIBS)
-  SET(BUILDNAME "${BUILDNAME}-Static")
-ENDIF(BUILD_SHARED_LIBS)
-
-# Add the build type, e.g. "Debug, Release..."
-IF(CMAKE_BUILD_TYPE)
-  SET(BUILDNAME "${BUILDNAME}-${CMAKE_BUILD_TYPE}")
-ENDIF(CMAKE_BUILD_TYPE)
