9664e8b1a27a128098a10334a0a5fd018303fe67
rleboulc <rleboulc@0f77665a-23fb-0310-bc23-e91fd8efc3e1>
2011-03-30 14:44:53 +0000

    Commit de Ronan Le Boulch : - Modifying anywave_table.h for doxygen
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mptk/trunk@1968 0f77665a-23fb-0310-bc23-e91fd8efc3e1

--- src/libmptk/anywave_table.h
+++ src/libmptk/anywave_table.h
@@ -57,238 +57,208 @@
  * use atoms with different sizes, you need several MP_Anywave_Table_c
  * instances.
 **/
-class MP_Anywave_Table_c {
-
-
-  /********/
-  /* DATA */
-  /********/
-
-public:
-  /** \brief Name of the file containing the waveform table (text
-   * file, extension .xml)
-   *
-   * To import the waveforms of the anywave atoms (e.g. generated with Matlab) in MPTK, you need to save them in two files as described below :
-   * - a xml file (text mode), describing all the parameters of the waveforms
-   * - a bin file (binary mode), containing the raw data
-   *
-   * The xml file, e.g. called "PATH/anywave.xml", must have the following syntax :
-   * - \<?xml version="1.0" encoding="ISO-8859-1"?\>
-   * - \<libVersion\>0.4beta\</libVersion\>
-   * - \<table\>
-   *   - \<par type = "numChans"\>2\</par\>
-   *   - \<par type = "filterLen"\>50\</par\>
-   *   - \<par type = "numFilters"\>10\</par\>
-   *   - \<par type = "data"\>PATH/anywave_data.bin\</par\>
-   * - \</table\>
-   *
-   * The associated binary file, called "PATH/anywave_data.bin", only
-   * contains the waveforms, in "double" format. The data is then \a
-   * numChans * \a numFilters * \a filterLen numbers in "double"
-   * format, in this example 1000 numbers, in the following order :
-   * - the first waveform, channel after channel
-   * - the second waveform, channel after channel 
-   * - so on ...
-   *
-   */
-  char tableFileName[MP_MAX_STR_LEN];
-  /** \brief Name of the file containing the data of the waveform table (binary file, extension .bin)
-   *
-   * To import the waveforms of the anywave atoms (e.g. generated with Matlab) in MPTK, you need to save them in two files as described below :
-   * - a xml file (text mode), describing all the parameters of the waveforms
-   * - a bin file (binary mode), containing the raw data
-   *
-   * The xml file, e.g. called "PATH/anywave.xml", must have the following syntax :
-   * - \<?xml version="1.0" encoding="ISO-8859-1"?\>
-   * - \<libVersion\>0.4beta\</libVersion\>
-   * - \<table\>
-   *   - \<par type = "numChans"\>2\</par\>
-   *   - \<par type = "filterLen"\>50\</par\>
-   *   - \<par type = "numFilters"\>10\</par\>
-   *   - \<par type = "data"\>PATH/anywave_data.bin\</par\>
-   * - \</table\>
-   *
-   * The associated binary file, called "PATH/anywave_data.bin", only
-   * contains the waveforms, in "double" format. The data is then \a
-   * numChans * \a numFilters * \a filterLen numbers in "double"
-   * format, in this example 1000 numbers, in the following order :
-   * - the first waveform, channel after channel
-   * - the second waveform, channel after channel 
-   * - so on ...
-   *
-   */
-  char dataFileName[MP_MAX_STR_LEN];
-
-  /** \brief Storage of the data. 
-   *
-   * The filters are stored one after the other, and for each filter,
-   * one channel after the other (as they are stored in the binary
-   * data file).
-   */
-  MP_Real_t* storage;
-
-  /** \brief Number of channels of the waveforms. 
-   * 
-   * It must be either 1, either the same number as in the signal to
-   * analyze.
-   */
-  MP_Chan_t numChans;
-
-  /** \brief Length of the waveforms
-   */
-  unsigned long int filterLen;
-
-  /** \brief Number of waveforms
-   */
-  unsigned long int numFilters;
-
-  /** \brief Flag indicating if the waveforms have been normalized
-   *
-   * 0 if not normalized
-   * 1 if normalized
-   * 2 if an error occured during the normalization.
-   */
-  unsigned long int normalized;
-  unsigned long int centeredAndDenyquisted;
-
-  /** \brief Table of pointers to the waveforms
-   *
-   * The table is indexed by the number of the filter and the number
-   * of the channel. By example, wave[2][1] points to the second
-   * channel of the third waveform
-  */
-  MP_Real_t*** wave;
-
-  /***********/
-  /* METHODS */
-  /***********/
-
-  /***************************/
-  /* CONSTRUCTORS/DESTRUCTOR */
-  /***************************/
-
-public:
-  /** \brief Default constructor 
-   */
-  MPTK_LIB_EXPORT MP_Anywave_Table_c( void );
-
-  /** \brief Constructor using a filename
-   *
-   * \param fileName A string containing the name of the file that
-   * describes the anywave table ("PATH/anywave.xml" in the example)
-   */
-  MPTK_LIB_EXPORT MP_Anywave_Table_c( char* fileName );
-
-  /** \brief Default destructor
-   */
-  MPTK_LIB_EXPORT virtual ~MP_Anywave_Table_c( void );
-
-  /***************************/
-  /* I/O METHODS             */
-  /***************************/
-
- public:
-  /** \brief Print the table structure to a stream
-   *
-   * the output is empty
-   *
-   * \param fid A writeable stream
-   * \remark DO NOT WRITE THE DATA TO the file \a dataFileName
-   */
-  MPTK_LIB_EXPORT void writeTable ( FILE *fidTable, const char *szDatasName );
-
-	/** \brief Print the table structure to a stream
-	 *
-	 * the output is the same as the file tableFileName
-	 *
-	 * \param fid A writeable stream
-	 * \remark DO NOT WRITE THE DATA TO the file \a dataFileName
-	 */
-	MPTK_LIB_EXPORT void writeDatas ( FILE *fidDatas );
+class MP_Anywave_Table_c 
+{
+	/********/
+	/* DATA */
+	/********/
+	public:
+		/** \brief Name of the file containing the waveform table (text
+		 * file, extension .xml)
+		 *
+		 * To import the waveforms of the anywave atoms (e.g. generated with Matlab) in MPTK, you need to save them in two files as described below :
+		 * - a xml file (text mode), describing all the parameters of the waveforms
+		 * - a bin file (binary mode), containing the raw data
+		 *
+		 * The xml file, e.g. called "PATH/anywave.xml", must have the following syntax :
+		 * - \<?xml version="1.0" encoding="ISO-8859-1"?\>
+		 * - \<libVersion\>0.4beta\</libVersion\>
+		 * - \<table\>
+		 *   - \<par type = "numChans"\>2\</par\>
+		 *   - \<par type = "filterLen"\>50\</par\>
+		 *   - \<par type = "numFilters"\>10\</par\>
+		 *   - \<par type = "data"\>PATH/anywave_data.bin\</par\>
+		 * - \</table\>
+		 *
+		 * The associated binary file, called "PATH/anywave_data.bin", only
+		 * contains the waveforms, in "double" format. The data is then \a
+		 * numChans * \a numFilters * \a filterLen numbers in "double"
+		 * format, in this example 1000 numbers, in the following order :
+		 * - the first waveform, channel after channel
+		 * - the second waveform, channel after channel 
+		 * - so on ...
+		 *
+		 */
+		char tableFileName[MP_MAX_STR_LEN];
+		/** \brief Name of the file containing the data of the waveform table (binary file, extension .bin)
+		 *
+		 * To import the waveforms of the anywave atoms (e.g. generated with Matlab) in MPTK, you need to save them in two files as described below :
+		 * - a xml file (text mode), describing all the parameters of the waveforms
+		 * - a bin file (binary mode), containing the raw data
+		 *
+		 * The xml file, e.g. called "PATH/anywave.xml", must have the following syntax :
+		 * - \<?xml version="1.0" encoding="ISO-8859-1"?\>
+		 * - \<libVersion\>0.4beta\</libVersion\>
+		 * - \<table\>
+		 *   - \<par type = "numChans"\>2\</par\>
+		 *   - \<par type = "filterLen"\>50\</par\>
+		 *   - \<par type = "numFilters"\>10\</par\>
+		 *   - \<par type = "data"\>PATH/anywave_data.bin\</par\>
+		 * - \</table\>
+		 *
+		 * The associated binary file, called "PATH/anywave_data.bin", only
+		 * contains the waveforms, in "double" format. The data is then \a
+		 * numChans * \a numFilters * \a filterLen numbers in "double"
+		 * format, in this example 1000 numbers, in the following order :
+		 * - the first waveform, channel after channel
+		 * - the second waveform, channel after channel 
+		 * - so on ...
+		 *
+		 */
+		char dataFileName[MP_MAX_STR_LEN];
+		/** \brief Storage of the data. 
+		 *
+		 * The filters are stored one after the other, and for each filter,
+		 * one channel after the other (as they are stored in the binary
+		 * data file).
+		 */
+		MP_Real_t* storage;
+		/** \brief Number of channels of the waveforms. 
+		 * 
+		 * It must be either 1, either the same number as in the signal to
+		 * analyze.
+		 */
+		MP_Chan_t numChans;
+		/** \brief Length of the waveforms */
+		unsigned long int filterLen;
+		/** \brief Number of waveforms */
+		unsigned long int numFilters;
+		/** \brief Flag indicating if the waveforms have been normalized
+		 * 0 if not normalized
+		 * 1 if normalized
+		 * 2 if an error occured during the normalization.
+		 */
+		unsigned long int normalized;
+		unsigned long int centeredAndDenyquisted;
+		/** \brief Table of pointers to the waveforms
+		 *
+		 * The table is indexed by the number of the filter and the number
+		 * of the channel. By example, wave[2][1] points to the second
+		 * channel of the third waveform
+		 */
+		MP_Real_t*** wave;
 	
-  /** \brief Print the table structure to a file
-   *
-   * the output is the same as the file tableFileName
-   *
-   * \param fName A string containing the file name
-   * \return The number of printed characters
-   * \remark DO NOT WRITE THE DATA TO the file \a dataFileName
-   */
-  MPTK_LIB_EXPORT unsigned long int write( const char *szTableName, const char *szDatasName );
+	/***********/
+	/* METHODS */
+	/***********/
 
-  /** \brief load the data contained in dataFileName, store it in
-   * storage and update the pointers in wave
-   *
-   * \return true for success, false for failure
-  **/
-  MPTK_LIB_EXPORT bool load_data( void );
+	/***************************/
+	/* CONSTRUCTORS/DESTRUCTOR */
+	/***************************/
+	public:
+		/** \brief Default constructor */
+		MPTK_LIB_EXPORT MP_Anywave_Table_c( void );
+		/** \brief Constructor using a filename
+		 * \param fileName A string containing the name of the file that
+		 * describes the anywave table ("PATH/anywave.xml" in the example)
+		 */
+		MPTK_LIB_EXPORT MP_Anywave_Table_c( char* fileName );
+		/** \brief Default destructor */
+		MPTK_LIB_EXPORT virtual ~MP_Anywave_Table_c( void );
 
+	/***************************/
+	/* I/O METHODS             */
+	/***************************/
+	public:
+		/** \brief Print the table structure to a stream
+		 *
+		 * the output is empty
+		 * \param fidTable A writeable stream
+		 * \param szDatasName The table data file name associated with the table file
+		 * \remark DO NOT WRITE THE DATA TO the file \a szDatasName
+		 */
+		MPTK_LIB_EXPORT void writeTable ( FILE *fidTable, const char *szDatasName );
+		/** \brief Print the table structure to a stream
+		 *
+		 * the output is the same as the file tableFileName
+		 *
+		 * \param fidDatas A writeable stream
+		 * \remark DO NOT WRITE THE DATA TO the file \a dataFileName
+		 */
+		MPTK_LIB_EXPORT void writeDatas ( FILE *fidDatas );
+		/** \brief Print the table structure to a file
+		 *
+		 * the output is the same as the file tableFileName
+		 *
+		 * \param szTableName The Anywave table file name string
+		 * \param szDatasName The Anywave table data file name string
+		 * \return The number of printed characters
+		 * \remark DO NOT WRITE THE DATA TO the file \a dataFileName
+		 */
+		MPTK_LIB_EXPORT unsigned long int write( const char *szTableName, const char *szDatasName );
+		/** \brief load the data contained in dataFileName, store it in
+		 * storage and update the pointers in wave
+		 *
+		 * \return true for success, false for failure
+		 **/
+		MPTK_LIB_EXPORT bool load_data( void );
+		/** \brief creates a copy of this table (with all allocations needed)
+		 * \return the copied anywave table
+		 */
+		MPTK_LIB_EXPORT MP_Anywave_Table_c* copy( void );
+		/** \brief creates the dual table, named name, containing, for each filter, its hilbert transform
+		 *
+		 * \param name The hilbert file name string
+		 * \return The created (hilbert) anywave table
+		 */
+		MPTK_LIB_EXPORT MP_Anywave_Table_c* create_hilbert_dual( char* name );
+	private:
+		/** \brief Parse the xml file fName that describes the table
+		 *
+		 * \return true for success, false for failure
+		 */
+		bool parse_xml_file(const char* fName);
 
-
-  /* Creates a copy of this table (with all allocations needed) */
-  MPTK_LIB_EXPORT MP_Anywave_Table_c* copy( void );
-
-  /* Creates the dual table, named name, containing, for each filter, its hilbert transform */
-  MPTK_LIB_EXPORT MP_Anywave_Table_c* create_hilbert_dual( char* name );
-
- private:
-  /** \brief Parse the xml file fName that describes the table
-   *
-   * \return true for success, false for failure
-   */
-  bool parse_xml_file(const char* fName);
-
-  /***************************/
-  /* OTHER METHODS           */
-  /***************************/
-  
- public:
-  /** \brief Test function, called by the test executable test_anywave
-   **/
-  MPTK_LIB_EXPORT static bool test( char* filename );
-  
-  /** \brief Initialize all the members
-   **/
-  MPTK_LIB_EXPORT void set_null( void );
-  
-  /** \brief Re-initialize all the members
-   **/
-  MPTK_LIB_EXPORT void reset( void );
-
-  /** \brief Normalize the waveforms and update the flag \a normalize
-   *
-   * \returns 1 if succeed or 2 if an error occured
-   */
-  MPTK_LIB_EXPORT unsigned long int normalize( void );
-
-  /** \brief Sets the mean and the nyquist component of the waveforms
-   * to 0, and update the flag \a centeredAndDenyquisted
-   *
-   * \returns 1 if succeed or 2 if an error occured
-   */
-  MPTK_LIB_EXPORT unsigned long int center_and_denyquist( void );
-
-  /** \brief set the \a tableFileName member to  \a fileName
-   * \return pointer to the string \a tableFileName
-   */
-  MPTK_LIB_EXPORT char* set_table_file_name( const char* fileName );
-
-  /** \brief set the \a dataFileName member to \a fileName
-   * \return pointer to the string \a dataFileName
-   */
-  MPTK_LIB_EXPORT char* set_data_file_name( const char* fileName );
-
- private:  
-  /** \brief Allocate the pointers array \a wave, using the dimensions
-   * \a numFilters and \a numChans
-   * 
-   * \return false if failed, true if succeed
-   **/
-  bool alloc_wave( void );
-
-  /** \brief Free the pointer array \a wave
-   **/
-  void free_wave( void );
+	/***************************/
+	/* OTHER METHODS           */
+	/***************************/
+	public:
+		/** \brief Test function, called by the test executable test_anywave
+		 *
+		 * \param filename The file to test in string
+		 */
+		MPTK_LIB_EXPORT static bool test( char* filename );
+		/** \brief Initialize all the members */
+		MPTK_LIB_EXPORT void set_null( void );
+		/** \brief Re-initialize all the members */
+		MPTK_LIB_EXPORT void reset( void );
+		/** \brief Normalize the waveforms and update the flag \a normalize
+		 *
+		 * \returns 1 if succeed or 2 if an error occured
+		 */
+		MPTK_LIB_EXPORT unsigned long int normalize( void );
+		/** \brief Sets the mean and the nyquist component of the waveforms
+		 * to 0, and update the flag \a centeredAndDenyquisted
+		 *
+		 * \returns 1 if succeed or 2 if an error occured
+		 */
+		MPTK_LIB_EXPORT unsigned long int center_and_denyquist( void );
+		/** \brief set the \a tableFileName member to  \a fileName
+		 * \return pointer to the string \a tableFileName
+		 */
+		MPTK_LIB_EXPORT char* set_table_file_name( const char* fileName );
+		/** \brief set the \a dataFileName member to \a fileName
+		 * \return pointer to the string \a dataFileName
+		*/
+		MPTK_LIB_EXPORT char* set_data_file_name( const char* fileName );
+	private:  
+		/** \brief Allocate the pointers array \a wave, using the dimensions
+		 * \a numFilters and \a numChans
+		 * 
+		 * \return false if failed, true if succeed
+		 **/
+		bool alloc_wave( void );
+		/** \brief Free the pointer array \a wave */
+		void free_wave( void );
 };
-
 #endif /* __anywave_table_h_ */
