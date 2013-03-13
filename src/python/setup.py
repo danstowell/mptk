70f39e26c0353d18bd9940969a16c22b5ba85f7e
Dan Stowell <danstowell@users.sourceforge.net>
2013-03-13 15:13:19 +0100

    pymptk: fixes for install on mac (use correct tinyxml path, and correct installed lib path)

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup,Extension
 
 src = ['pyMPTK.cpp', 'pyMPTK_book.cpp', 'pyMPTK_decompose.cpp', 'pyMPTK_atom.cpp']
 
-inc_dir = ['/usr/include', '@MPTK_SOURCE_DIR@','@MPTK_SOURCE_DIR@/src/tinyxml', '@MPTK_SOURCE_DIR@/src/libmptk/', '@MPTK_BINARY_DIR@' , '@MPTK_BINARY_DIR@/src/libmptk/', '@LIBDSP_INCLUDE_DIR@','@FFTW3_INCLUDE_DIR@', '@SNDFILE_INCLUDE_DIR@']
+inc_dir = ['/usr/include', '@MPTK_SOURCE_DIR@','@MPTK_SOURCE_DIR@/src/utils/libtinyxml', '@MPTK_SOURCE_DIR@/src/libmptk/', '@MPTK_BINARY_DIR@' , '@MPTK_BINARY_DIR@/src/libmptk/', '@LIBDSP_INCLUDE_DIR@','@FFTW3_INCLUDE_DIR@', '@SNDFILE_INCLUDE_DIR@']
 
-lib =['@MPTK_BINARY_DIR@/lib/@LIB_MPTK_OUTPUT_NAME@','@MPTK_BINARY_DIR@/lib/dsp_windows']
+lib =['@CMAKE_INSTALL_PREFIX@/lib/@LIB_MPTK_OUTPUT_NAME@','@CMAKE_INSTALL_PREFIX@/lib/dsp_windows']
 
 setup (name = "mptk",
 	version = "0.1",
